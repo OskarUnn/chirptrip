d80147c06d2bd57e36cbc285c7fabc8a83525eeb
OskarUnn <oskar.unn@gmail.com>
2025-03-11 13:32:08 +0200

    initial project setup

---
+++ backend/src/main/java/chirptrip/backend/ChirpTripApplication.java
@@ -0,0 +1,13 @@
+package chirptrip.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChirpTripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChirpTripApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/chirptrip/backend/ChirpTripApplicationTests.java
@@ -0,0 +1,13 @@
+package chirptrip.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChirpTripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
