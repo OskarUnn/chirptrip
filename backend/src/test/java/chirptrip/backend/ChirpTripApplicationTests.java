cdf757c4ce3bd8f6f8cbd78665c8c34abd4d24d4
OskarUnn <oskar.unn@gmail.com>
2025-03-14 19:56:29 +0200

    containerize backend

--- backend/src/test/java/chirptrip/backend/ChirpTripApplicationTests.java
+++ backend/src/test/java/chirptrip/backend/ChirpTripApplicationTests.java
@@ -2,8 +2,10 @@ package chirptrip.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
 class ChirpTripApplicationTests {
 
 	@Test
